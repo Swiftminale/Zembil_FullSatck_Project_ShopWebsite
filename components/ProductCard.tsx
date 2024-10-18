a447aa9c18f2f049edd04bc2da2d149416d9255b
Swiftminale <minalefetene@gmail.com>
2024-10-18 05:08:19 +0300

    working on cart

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,60 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/users");
+      const data = await res.json();
+      setSignedInUser(data);
+      setIsLiked(data.wisjlist.includes(product._id));
+      setLoading(false);
+    } catch (err) {
+      console.log("[user_GET", err);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, [user]);
+
+  const handleLike = async (
+    e: React.MouseEvent<HTMLBodyElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    try {
+      if (!user) {
+        router.push("/sign-in");
+        return;
+      }else{
+      setLoading(true);
+      const res = await fetch("/api/users/wishlist", {
+        method: "POST",
+        body: JSON.stringify({ productId: product._id }),
+      });
+      const updatedUser = await res.json();
+      setSignedInUser(updatedUser);
+      setIsLiked(updatedUser.wishlist.includes(product._id));}
+    } catch (err) {
+      console.log("[wichlist_POST]", err);
+    }
+  };
   return (
     <Link
       href={`/products/${product._id}`}
@@ -22,8 +73,8 @@ const ProductCard = ({ product }: { product: ProductType }) => {
       </div>
       <div className="flex justify-between items-center">
         <p className="text-body-bold">Br {product.price}</p>
-        <button>
-          <Heart />
+        <button onClick={handleLike}>
+          <Heart fill={`${isLiked ? "red" : "white"}`} />
         </button>
       </div>
     </Link>
--- lib/action.ts
+++ lib/action.ts
@@ -19,8 +19,45 @@ export const getCollections = async () => {
   }
 };
 
-
 export const getProducts = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
-    return await response.json();
-}
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
+  return await response.json();
+};
+
+export const getProductsDetails = async (productsId: string) => {
+  const product = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productsId}`
+  );
+
+  // Log the response text to inspect it
+  const text = await product.text();
+  console.log("Response:", text);
+
+  // Attempt to parse JSON only if the response looks like JSON
+  try {
+    return JSON.parse(text);
+  } catch (error) {
+    console.error("Failed to parse JSON:", error);
+  }
+};
+
+export const getSearchedProducts = async (query: string) => {
+  const searchedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/search/${query}`
+  );
+  return await searchedProducts.json();
+};
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`
+  );
+  return await orders.json();
+};
+
+export const getRelatedProducts = async (productId: string) => {
+  const relatedProducts = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`
+  );
+  return await relatedProducts.json();
+};
